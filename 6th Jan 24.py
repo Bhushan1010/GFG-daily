63e9ffaad1808bb8442f40a34410eece105c1945
Bhushan1010 <bhushansisode11@gmail.com>
2024-01-06 15:39:35 +0530

    Code of the Day

---
+++ 6th Jan 24.py
@@ -0,0 +1,14 @@
+from typing import List
+class Solution:
+    def sumOfPowers(self, a : int, b : int) -> int:
+        x = [i for i in range(b+1)]
+        for i in range(2, int(b**0.5)+1):
+            if x[i] == i:
+                for j in range(i*i, b+1, i):
+                    x[j] = i
+        y = 0
+        for n in range(a, b+1):
+            while n > 1:
+                y = y + 1
+                n = n // x[n]
+        return y
